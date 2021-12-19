4369656c8d8d5b0d624bc5a9c432fb43e95e6fba
Mars Zuo <marszuo@foxmail.com>
2021-12-19 11:05:31 +0800

    feat: Add command line client

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod block;
 use block::Block;
 
 mod blockchain;
+pub use blockchain::Blockchain;
 
 mod proof_of_work;
 use proof_of_work::ProofOfWork;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use blockchain_rust::Blockchain;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "blockchain_rust")]
+struct Opt {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    #[structopt(name = "addblock", about = "Add new block to chain")]
+    Addblock {
+        #[structopt(name = "data", help = "The string value of the block data")]
+        data: String,
+    },
+    #[structopt(name = "printchain", about = "Print blockchain all block")]
+    Printchain,
+}
+
 fn main() {
-    println!("hello rust")
+    let opt = Opt::from_args();
+    match opt.command {
+        Command::Addblock { data } => {
+            let mut blockchain = Blockchain::new_blockchain();
+            blockchain.add_block(data);
+        }
+        Command::Printchain => {
+            let mut block_iterator = Blockchain::new_blockchain().iterator();
+            loop {
+                let option = block_iterator.next();
+                if option.is_none() {
+                    break;
+                }
+                let block = option.unwrap();
+                println!("Pre block hash: {}", block.get_pre_block_hash());
+                println!("Cur block hash: {}", block.get_hash());
+                println!("Data: {}", block.get_data());
+                println!("Timestamp: {}\n", block.get_timestamp());
+            }
+        }
+    }
 }
