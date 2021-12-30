92d33c190bfe0fb8181e1ab0ca1b01a77a94fe2a
Mars Zuo <marszuo@foxmail.com>
2021-12-30 17:25:13 +0800

    feat: add client test case

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,24 +1,75 @@
-use blockchain_rust::{Blockchain, UTXOSet, Wallets};
-use ring::io::der::Tag::UTCTime;
-use std::env::current_dir;
+use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
 use std::process::Command;
 
 #[test]
 fn client_createblockchain() {
-    let blockchain = Blockchain::create_blockchain();
-    let _ = UTXOSet::new(blockchain);
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("createblockchain")
+        .assert()
+        .success();
 }
 
 #[test]
 fn client_createwallet() {
-    let mut wallets = Wallets::new();
-    let address = wallets.create_wallet();
-    println!("{}", address)
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("createwallet")
+        .assert()
+        .success();
 }
 
 #[test]
 fn client_get_addresses() {
-    let wallets = Wallets::new();
-    let addresses = wallets.get_addresses();
-    println!("{:?}", addresses)
+    let assert = Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("listaddresses")
+        .assert()
+        .success();
+
+    let output_bytes = assert.get_output().stdout.as_slice();
+    println!("{}", String::from_utf8(output_bytes.to_vec()).unwrap())
+}
+
+#[test]
+fn client_get_balance() {
+    let command = Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("getbalance")
+        .arg("1Mr2LYb7XcbKe2Ck9po5zZi69SEnAK7Wk6")
+        .assert()
+        .success();
+
+    let output_bytes = command.get_output().stdout.as_slice();
+    println!("{}", String::from_utf8(output_bytes.to_vec()).unwrap())
+}
+
+#[test]
+fn client_send() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("send")
+        .arg("1Mr2LYb7XcbKe2Ck9po5zZi69SEnAK7Wk6")
+        .arg("1C2mcxwiNmDs6RfagFiPNgSo9KQoi4oguY")
+        .arg("5")
+        .assert()
+        .success();
+}
+
+#[test]
+fn client_printchain() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("printchain")
+        .assert()
+        .success();
+}
+
+#[test]
+fn client_reindexutxo() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("reindexutxo")
+        .assert()
+        .success();
 }
